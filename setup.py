2896250936452a92172b21b525dd30b423a23257
mwood1212 <xz17823@bristol.ac.uk>
2021-08-28 23:46:53 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+import setuptools
 
 with open ("README.md", "r") as fh :
 	long_description = fh.read ()
 
 setup (
-	name="AutoCTR",
+	name="autoctr",
 	version="0.0.1",
 	author="Zhengxin Fang",
 	author_email="358746595@qq.com",
