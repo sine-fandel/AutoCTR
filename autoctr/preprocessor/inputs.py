81975722ee8b2578b2a069692af2321a945aa2cb
mwood1212 <xz17823@bristol.ac.uk>
2021-09-06 01:35:30 +0800

    finished feature return

--- autoctr/preprocessor/inputs.py
+++ autoctr/preprocessor/inputs.py
@@ -69,16 +69,16 @@ class Input (object) :
 
 
 
-def run (data_path, outlier='z_score', correlation='pearson'):
-	"""Entry point for console_scripts
-	"""
+# def run (data_path, outlier='z_score', correlation='pearson'):
+# 	"""Entry point for console_scripts
+# 	"""
 
-	profiling = Input (data_path=data_path, sep=",")
+# 	profiling = Input (data_path=data_path, sep=",")
 
-	profiling.preprocessing ()
+# 	profiling.preprocessing ()
 
 
-if __name__ == "__main__" :
-	# data = pd.read_csv('/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
+# if __name__ == "__main__" :
+# 	# data = pd.read_csv('/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
 
-	run (data_path='/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
+# 	run (data_path='/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
