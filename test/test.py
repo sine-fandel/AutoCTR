d6ee341228d4f4304b19464146e684e6e2873b91
mwood1212 <xz17823@bristol.ac.uk>
2021-09-06 01:41:43 +0800

    finished the data preprocessing of system

--- test/test.py
+++ test/test.py
@@ -4,12 +4,16 @@ import pandas as pd
 def run (data_path, outlier='z_score', correlation='pearson'):
 	"""Entry point for console_scripts
 	"""
-	data = pd.read_csv (data_path, sep='::')
-	profiling = Input (data)
 
-	print (profiling.preprocessing ())
+	profiling = Input (data_path=data_path, sep=",")
+
+	profiling.preprocessing ()
 
 
 if __name__ == "__main__" :
-	run (data_path='/Users/apple/AutoCTR project/dataset/Movielens/ml-1m/ratings.dat')    
-	# run (data_path='/Users/apple/project/AI + elearning project/data_profiling/test/west.csv')
+	# data = pd.read_csv('/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
+
+	run (data_path='/Users/apple/Downloads/tf-experiment/criteo_sample.txt')
+
+
+	
