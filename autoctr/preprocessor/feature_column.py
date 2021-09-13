ff23ae7956444ee11e75397225f388c41154058f
mwood1212 <xz17823@bristol.ac.uk>
2021-09-13 09:43:41 +0800

    add several functions

--- autoctr/preprocessor/feature_column.py
+++ autoctr/preprocessor/feature_column.py
@@ -88,3 +88,86 @@ class DenseFeat(namedtuple('DenseFeat', ['name', 'dimension', 'dtype'])):
 
 	def __hash__(self):
 		return self.name.__hash__()
+
+def build_input_features(feature_columns):
+	# Return OrderedDict: {feature_name:(start, start+dimension)}
+
+	features = OrderedDict()
+
+	start = 0
+	for feat in feature_columns:
+		feat_name = feat.name
+		if feat_name in features:
+			continue
+		if isinstance(feat, SparseFeat):
+			features[feat_name] = (start, start + 1)
+			start += 1
+		elif isinstance(feat, DenseFeat):
+			features[feat_name] = (start, start + feat.dimension)
+			start += feat.dimension
+		elif isinstance(feat, VarLenSparseFeat):
+			features[feat_name] = (start, start + feat.maxlen)
+			start += feat.maxlen
+			if feat.length_name is not None and feat.length_name not in features:
+				features[feat.length_name] = (start, start + 1)
+				start += 1
+		else:
+			raise TypeError("Invalid feature column type,got", type(feat))
+
+	return features
+
+def get_varlen_pooling_list(embedding_dict, features, feature_index, varlen_sparse_feature_columns, device):
+	varlen_sparse_embedding_list = []
+	for feat in varlen_sparse_feature_columns:
+		seq_emb = embedding_dict[feat.name]
+		if feat.length_name is None:
+			seq_mask = features[:, feature_index[feat.name][0]:feature_index[feat.name][1]].long() != 0
+
+			emb = SequencePoolingLayer(mode=feat.combiner, supports_masking=True, device=device)(
+				[seq_emb, seq_mask])
+		else:
+			seq_length = features[:, feature_index[feat.length_name][0]:feature_index[feat.length_name][1]].long()
+			emb = SequencePoolingLayer(mode=feat.combiner, supports_masking=False, device=device)(
+				[seq_emb, seq_length])
+		varlen_sparse_embedding_list.append(emb)
+	return varlen_sparse_embedding_list
+
+def create_embedding_matrix(feature_columns, init_std=0.0001, linear=False, sparse=False, device='cpu'):
+	# Return nn.ModuleDict: for sparse features, {embedding_name: nn.Embedding}
+	# for varlen sparse features, {embedding_name: nn.EmbeddingBag}
+	sparse_feature_columns = list(
+		filter(lambda x: isinstance(x, SparseFeat), feature_columns)) if len(feature_columns) else []
+
+	varlen_sparse_feature_columns = list(
+		filter(lambda x: isinstance(x, VarLenSparseFeat), feature_columns)) if len(feature_columns) else []
+
+	embedding_dict = nn.ModuleDict(
+		{feat.embedding_name: nn.Embedding(feat.vocabulary_size, feat.embedding_dim if not linear else 1, sparse=sparse)
+		for feat in
+		sparse_feature_columns + varlen_sparse_feature_columns}
+	)
+
+	# for feat in varlen_sparse_feature_columns:
+	#     embedding_dict[feat.embedding_name] = nn.EmbeddingBag(
+	#         feat.dimension, embedding_size, sparse=sparse, mode=feat.combiner)
+
+	for tensor in embedding_dict.values():
+		nn.init.normal_(tensor.weight, mean=0, std=init_std)
+
+	return embedding_dict.to(device)
+
+def varlen_embedding_lookup(X, embedding_dict, sequence_input_dict, varlen_sparse_feature_columns):
+    varlen_embedding_vec_dict = {}
+    for fc in varlen_sparse_feature_columns:
+        feature_name = fc.name
+        embedding_name = fc.embedding_name
+        if fc.use_hash:
+            # lookup_idx = Hash(fc.vocabulary_size, mask_zero=True)(sequence_input_dict[feature_name])
+            # TODO: add hash function
+            lookup_idx = sequence_input_dict[feature_name]
+        else:
+            lookup_idx = sequence_input_dict[feature_name]
+        varlen_embedding_vec_dict[feature_name] = embedding_dict[embedding_name](
+            X[:, lookup_idx[0]:lookup_idx[1]].long())  # (lookup_idx)
+
+    return varlen_embedding_vec_dict
