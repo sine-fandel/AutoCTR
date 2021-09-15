ec90af67bf9f54cc6650f824ae6d9b0c3a2090f8
mwood1212 <xz17823@bristol.ac.uk>
2021-09-15 18:15:05 +0800

    new test instance

--- test/test.py
+++ test/test.py
@@ -1,20 +1,27 @@
-from autoctr.preprocessor.inputs import Input
-import pandas as pd
-from autoctr.models import *
-from sklearn.metrics import log_loss, roc_auc_score
+# from autoctr.preprocessor.inputs import Input
+# import pandas as pd
+# from autoctr.models import *
+# from sklearn.metrics import log_loss, roc_auc_score
 
-data_path='/Users/apple/Downloads/tf-experiment/criteo_sample.txt'
+# data_path = '/Users/apple/Downloads/tf-experiment/criteo_sample.txt'
+# target = 'label'
+# profiling = Input (data_path=data_path, sep=",", target=target)
 
-profiling = Input (data_path=data_path, sep=",")
+# train, test, train_model_input, test_model_input, linear_feature_columns, dnn_feature_columns = profiling.preprocessing (impute_method='iterative')
+# model_list = [DeepFM, AutoInt]
 
-train, test, train_model_input, test_model_input, linear_feature_columns, dnn_feature_columns = profiling.preprocessing ()
-model = DeepFM (linear_feature_columns=linear_feature_columns, dnn_feature_columns=dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device='cpu')
-model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
-model.fit (train_model_input, train['label'].values, batch_size=32, epochs=10, verbose=2, validation_split=0.8)
-pred_ans = model.predict (test_model_input, 256)
+# for Model in model_list :
+# 	model = Model (linear_feature_columns=linear_feature_columns, dnn_feature_columns=dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device='cpu')
+# 	model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
+# 	model.fit (train_model_input, train[target].values, batch_size=32, epochs=100, verbose=2, validation_split=0.8)
+# 	pred_ans = model.predict (test_model_input, 256)
 
-print ("test LogLoss", round (log_loss(test['label'].values, pred_ans), 4))
-print ("test AUC", round (roc_auc_score(test['label'].values, pred_ans), 4))
+# 	print ("test LogLoss", round (log_loss(test['label'].values, pred_ans), 4))
+# 	print ("test AUC", round (roc_auc_score(test['label'].values, pred_ans), 4))
 
+from autoctr.process import AutoCTR
 
-	
+rec = AutoCTR (data_path='/Users/apple/Downloads/tf-experiment/criteo_sample.txt', target='label')
+rec.preprocessing ()
+rec.run (epochs=10)
+
