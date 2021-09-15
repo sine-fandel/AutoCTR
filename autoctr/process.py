5321f121eecdac4156d6a99ba116eaa391bec5aa
mwood1212 <xz17823@bristol.ac.uk>
2021-09-15 18:13:50 +0800

    add more models and change the split method

--- autoctr/process.py
+++ autoctr/process.py
@@ -13,15 +13,15 @@ class AutoCTR :
 	def __init__ (self, data_path, target, seq=",") :
 		self.data_path = data_path
 		self.target = target
-		self.model_list = [DeepFM, AutoInt, AFN]
+		self.model_list = [DeepFM, xDeepFM, AFN, NFM, IFM, DIFM, AutoInt, PNN, DCN, DCNMix, ONN, WDL]
 		self.seq = seq
 		self.input_list = []
 
-	def preprocessing (self) :
-		profiling = Input (data_path=self.data_path, sep=self.seq)
+	def preprocessing (self, test_size=0.2) :
+		profiling = Input (data_path=self.data_path, sep=self.seq, test_size=test_size)
 		self.input_list = profiling.preprocessing (impute_method='iterative')
 
-	def run (self, batch_size=32, epochs=100, verbose=2, validation_split=0.8) :
+	def run (self, batch_size=32, epochs=100, verbose=2) :
 		train = self.input_list[0]
 		test = self.input_list[1]
 		train_model_input = self.input_list[2]
@@ -30,12 +30,23 @@ class AutoCTR :
 		dnn_feature_columns = self.input_list[5]
 
 		for Model in self.model_list :
-			model = Model (linear_feature_columns=linear_feature_columns, dnn_feature_columns=dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device='cpu')
-			model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
-			model.fit (train_model_input, train[self.target].values, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_split=validation_split)
-			pred_ans = model.predict (test_model_input, 256)
+			print("Train on {0} samples, validate on {1} samples".format (len(train), len(test)))
+			if Model.__name__ != "PNN" :
 
-			print ("test AUC:", round (roc_auc_score(test[self.target].values, pred_ans), 4))
+				model = Model (linear_feature_columns=linear_feature_columns, dnn_feature_columns=dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device='cpu')
+				model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
+				model.fit (train_model_input, train[self.target].values, batch_size=batch_size, epochs=epochs, verbose=verbose)
+				pred_ans = model.predict (test_model_input, 256)
+
+				print ("Validation Accuracy: ", round (roc_auc_score(test[self.target].values, pred_ans), 4))
+			
+			else :
+				model = Model (dnn_feature_columns=dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device='cpu')
+				model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
+				model.fit (train_model_input, train[self.target].values, batch_size=batch_size, epochs=epochs, verbose=verbose)
+				pred_ans = model.predict (test_model_input, 256)
+
+				print ("Validation Accuracy: ", round (roc_auc_score(test[self.target].values, pred_ans), 4))
 
 
 	
