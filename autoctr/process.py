a0ea08ea0a477d6d68f20daf78e3fae9c831aade
mwood1212 <xz17823@bristol.ac.uk>
2021-09-17 18:04:02 +0800

    transfer preprocessor/inputs.py functions to the entrance of AutoCTR

--- autoctr/process.py
+++ autoctr/process.py
@@ -1,9 +1,26 @@
+# -*- coding:utf-8 -*-
+"""
+
+Author: 
+	Zhengxin Fang, 358746595@qq.com
+
+The extrance of AutoCTR package.
+
+"""
 from .preprocessor.inputs import Input
-import pandas as pd
+from .preprocessor.profile import Profiling
+from .preprocessor.cleaning import Impute
+from .preprocessor.feature_column import SparseFeat, DenseFeat
 from .models import *
+
 from sklearn.metrics import log_loss, roc_auc_score, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 import torch
 import os
+import pandas as pd
+import numpy as np
 
 class AutoCTR :
 	"""The whole process of recommender
@@ -13,17 +30,87 @@ class AutoCTR :
 	:param sep: The seq of the dataset
 	"""
 	def __init__ (self, data_path, target, sep=",") :
-		self.data_path = data_path
+		self.data = pd.read_csv (data_path, sep=sep)
 		self.target = target
 		self.model_list = [DeepFM, xDeepFM, AFN, NFM, IFM, DIFM, AutoInt, PNN, DCN, DCNMix, ONN, WDL]
 		self.sep = sep
 		self.input_list = []
+		self.data_profile = None
+		self.types_dict = {}
+		self.tag = 0
 
-	def preprocessing (self, test_size=0.2) :
-		profiling = Input (data_path=self.data_path, sep=self.sep, target=self.target, test_size=test_size)
-		self.input_list = profiling.preprocessing (impute_method='iterative')
+	def profiling (self, test_size=0.2, outlier="z_score", correlation="pearson") :
+		"""Get the data summary of the dataset.
+		"""
+		self.data_profile = Profiling (self.data, outlier=outlier, correlation=correlation).summary ()
+		self.types_dict = self.data_profile.loc['types'].to_dict ()
+		self.tag = 1
+
+	def preprocessing (self, impute_method="knn", test_size=0.2) :
+		"""Deal with the missing values and convert data for training
+		"""
+		try :
+			if not self.tag :
+				raise Exception ("[Error]: Do not get dataset yet...")
+
+			types_dict = self.data_profile.loc['types'].to_dict ()
+			count_missing = self.data_profile.loc['missing'].to_dict ()
+			if_missing = 0
+			for key, value in count_missing.items () :
+				if value != 0 :
+					if_missing = 1
+
+			if if_missing :
+				impute = Impute (self.data)
+				if impute_method == 'knn' :
+					self.data = impute.KnnImputation (n_neighbors=2)
+				elif impute_method == 'simple' :
+					self.data = impute.SimpleImputation ()
+				elif impute_method == 'iterative' :
+					self.data = impute.IterativeImputation ()
+				elif impute_method == 'forest' :
+					self.data = impute.RandomforestImputation ()
+				elif impute_method == 'mf' :
+					self.data = impute.MatrixFactorization ()
+
+				print ("Finish imputation by ", impute_method)
+			else :
+				print ("No missing value. Nothing to do...")
+
+			feature_names = self.data_profile.columns.values
+			feature_names_temp = list (feature_names)
+			feature_names = np.delete (feature_names, feature_names_temp.index (self.target))
+
+			fixlen_feature_columns = []
+
+			for key, value in types_dict.items () :
+				if key != self.target :
+					if value == "categorical" :
+						lbe = LabelEncoder ()
+						self.data[key] = lbe.fit_transform (self.data[key])
+						fixlen_feature_columns.append (SparseFeat (key, self.data[key].nunique ()))
+						
+					elif value == "numeric" :
+						fixlen_feature_columns.append (DenseFeat (key, 1, ))
+			
+			train, test = train_test_split (self.data, test_size=test_size, random_state=2021)
+			self.input_list.append (train)
+			self.input_list.append (test)
+			self.input_list.append ({name: train[name] for name in feature_names})
+			self.input_list.append ({name: test[name] for name in feature_names})
+			self.input_list.append (fixlen_feature_columns)
+			self.input_list.append (fixlen_feature_columns)
+
+			print ("Have converted data to training format...")
+
+		except Exception as e :
+			print (e)
+			
 
 	def run (self, batch_size=32, epochs=100, verbose=2, save_path="./PKL/", earl_stop_patience=0) :
+		"""Train and Test
+		"""
+		
 		if not os.path.exists (save_path) :
 			os.makedirs (save_path)
 
