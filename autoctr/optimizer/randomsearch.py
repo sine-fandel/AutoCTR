91cb4a656a22f6e245f4972933bf2f5118440281
mwood1212 <xz17823@bristol.ac.uk>
2021-09-20 20:33:18 +0800

    finished Random search part

--- autoctr/optimizer/randomsearch.py
+++ autoctr/optimizer/randomsearch.py
@@ -14,6 +14,8 @@ from alive_progress import alive_bar
 
 from sklearn.metrics import log_loss, roc_auc_score, mean_squared_error
 
+from ..models import *
+
 
 class RandomSearch () :
 	"""Random search for hp tuning
@@ -21,8 +23,8 @@ class RandomSearch () :
 	:param models: Model list of tuning
 	:param max_evals: The max rounds of tuning
 	"""
-	def __init__ (self, Model, hp_grid, linear_feature_columns, dnn_feature_columns, task="binary", device="cpu", max_evals=10) :
-		self.Model = Model
+	def __init__ (self, model_name, hp_grid, linear_feature_columns, dnn_feature_columns, task="binary", device="cpu", max_evals=10) :
+		self.model_name = model_name
 		self.max_evals = max_evals
 		self.linear_feature_columns = linear_feature_columns
 		self.dnn_feature_columns = dnn_feature_columns
@@ -36,11 +38,61 @@ class RandomSearch () :
 		with alive_bar (self.max_evals) as bar :
 			for i in range (self.max_evals) :
 				random_params = {k: random.sample (v.tolist (), 1)[0] if not isinstance (v, tuple) else (random.sample (v[0].tolist (), 1)[0], random.sample (v[1].tolist (), 1)[0]) for k, v in self.hp_grid.items ()}
-
-				model = self.Model (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
-									task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
-									l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
-									l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				
+				if self.model_name == "DeepFM" :
+					model = DeepFM (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "xDeepFM" :
+					model = xDeepFM (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, cin_layer_size=random_params['cin_layer_size'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'], l2_reg_cin=random_params['l2_reg_cin'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "IFM" :
+					model = IFM (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "DIFM" :
+					model = DIFM (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "NFM" :
+					model = NFM (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'], bi_dropout=random_params['bi_dropout'])
+				elif self.model_name == "ONN" :
+					model = ONN (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "PNN" :
+					model = PNN (dnn_feature_columns=self.dnn_feature_columns, task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_embedding=random_params['l2_reg_embedding'], l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "DCN" :
+					model = DCN (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'], l2_reg_cross=random_params['l2_reg_cross'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "DCNMix" :
+					model = DCNMix (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'], l2_reg_cross=random_params['l2_reg_cross'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'],
+										low_rank=random_params['low_rank'], num_experts=random_params['num_experts'])
+				elif self.model_name == "AFN" :
+					model = AFN (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										task=self.task, device=self.device, ltl_hidden_size=random_params['ltl_hidden_size'], afn_dnn_hidden_units=random_params['afn_dnn_hidden_units'],
+										l2_reg_linear=random_params['l2_reg_linear'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
+				elif self.model_name == "AutoInt" :
+					model = AutoInt (linear_feature_columns=self.linear_feature_columns, dnn_feature_columns=self.dnn_feature_columns, 
+										att_layer_num=random_params['att_layer_num'], att_head_num=random_params['att_head_num'],
+										task=self.task, device=self.device, dnn_hidden_units=random_params['dnn_hidden_units'], l2_reg_embedding=random_params['l2_reg_embedding'],
+										l2_reg_dnn=random_params['l2_reg_dnn'], init_std=random_params['init_std'], dnn_dropout=random_params['dnn_dropout'])
 
 				model.compile ("adagrad", "binary_crossentropy", metrics=["binary_crossentropy", "auc"], )
 				model.fit (train_model_input, train_y, epochs=epochs, verbose=verbose, earl_stop_patience=earl_stop_patience, if_tune=1)
@@ -54,8 +106,8 @@ class RandomSearch () :
 				bar ()
 				bar.text ("#%d  Accuracy: %.4f" % (i + 1, cur_score))
 
-		print (best_Score)
-		print (best_param)
+		print ("Best Accuracy: %.4f" % (best_Score))
+		print ("Best Hyperparameters: ", (best_param))
 
 
 
