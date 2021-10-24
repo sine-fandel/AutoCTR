b7544e83f564719147ee05f379a0f4b6b724ad98
mwood1212 <xz17823@bristol.ac.uk>
2021-10-24 10:42:31 +0800

    change a bit

--- autoctr/preprocessor/quality.py
+++ autoctr/preprocessor/quality.py
@@ -24,7 +24,7 @@ class QoD (object) :
 	def __init__ (self, df, target='label') :
 		self.df = df
 		self.length = len (self.df)
-		self.label = target
+		self.target = target
 
 	def Plot (self, score, title) :
 		# just finished the missing value detect
@@ -46,7 +46,7 @@ class QoD (object) :
 			plt.text (x=-0.2, y=-0.08, s=value, fontsize=12)
 			plt.title (key)
 
-		plt.savefig ('./' + title + '_Score.png')
+		# plt.savefig ('./' + title + '_Score.png')
 
 	def _get_outlier (self, method="z_score") :
 		"""Get outlier score
@@ -57,7 +57,7 @@ class QoD (object) :
 		temp = 0
 		counts = 0
 		for c in self.df.columns :
-			if self.df[c].dtype != 'object' and c != self.label:
+			if self.df[c].dtype != 'object' and c != self.target:
 				if method == 'z_score' :
 					count = 0
 					counts += self.df[c].count ()
@@ -86,12 +86,12 @@ class QoD (object) :
 		count = 0
 		counts = 0
 		for c in self.df.columns :
-			if c != self.label :
+			if c != self.target :
 				count += len (self.df[c]) - self.df[c].isnull().sum()
 				counts += self.length
 
 		score['Data Completeness'] = round (count / counts, 2)
-
+		print ("Missing number: ", counts - count)
 		self.Plot (score, "Completeness")
 
 		return score
@@ -105,7 +105,7 @@ class QoD (object) :
 
 		score = {}
 		score['Data Duplicated'] = round (duplicated_count[False] / self.length, 2)
-
+		print ("Duplicated number: ", self.length - duplicated_count[False])
 		self.Plot (score, "Duplicated")
 
 		return score
@@ -117,17 +117,17 @@ class QoD (object) :
 			parity score = (class imbalance + class purity) / 2
 		:param method: the method of getting outliers
 		"""
-		label_counts_list = [value for value in Counter (self.df[self.label].values).values ()]
+		label_counts_list = [value for value in Counter (self.df[self.target].values).values ()]
 
 		noisy_count = 0
-		noisy_count += self.df[self.label].isnull().sum()
+		noisy_count += self.df[self.target].isnull().sum()
 		if method == 'z_score' :
 			count = 0
 			label_zscore = {}
-			z_score = (np.array (self.df[self.label]) - np.mean (self.df[self.label])) / np.std (self.df[self.label])
+			z_score = (np.array (self.df[self.target]) - np.mean (self.df[self.target])) / np.std (self.df[self.target])
 
 			n = 0
-			for l in self.df[self.label].values :
+			for l in self.df[self.target].values :
 				label_zscore[l] = z_score[n]
 				n += 1
 			
@@ -163,7 +163,7 @@ class QoD (object) :
 	def _get_correlations (self, method="pearson") :
 		"""Get correlation between the data
 		"""
-		# data = self.df.drop (labels=self.label, axis=1)
+		# data = self.df.drop (labels=self.target, axis=1)
 		cat_col = []
 		for c in self.df.columns :
 			if self.df[c].dtypes == "object" :
@@ -216,8 +216,8 @@ class QoD (object) :
 
 			cor_result = self.df.corr (method=method)
 
-		cor_result = cor_result.drop (labels=self.label, axis=1)
-		cor_result = cor_result.drop (labels=self.label, axis=0)
+		cor_result = cor_result.drop (labels=self.target, axis=1)
+		cor_result = cor_result.drop (labels=self.target, axis=0)
 		cor_result = np.abs (cor_result) > 0.5
 		high_cor_count = 0
 		for row in cor_result.values :
@@ -234,10 +234,49 @@ class QoD (object) :
 
 		return score
 
+	def plot_sparse (self, col) :
+		"""Plot the figure of sparse of each columns
+		:param col: the column that to be plot
+		"""
+		stats = self.df[[col, self.target]].groupby (col).agg (['count', 'mean', 'sum'])
+		stats = stats.reset_index ()
+		stats.columns = [col, 'count', 'mean', 'sum']
+		stats_sort = stats['count'].value_counts ().reset_index ()
+		stats_sort = stats_sort.sort_values ('index')
+		plt.figure (figsize=(15,4))
+		plt.bar (stats_sort['index'].astype (str).values[0:20], stats_sort['count'].values[0:20])
+		plt.title ('Frequency of ' + str(col))
+		plt.xlabel ('Number frequency')
+		plt.ylabel ('Frequency')
 
-# data = pd.read_csv ("/Users/apple/AutoCTR project/AutoCTR/autoctr/criteo_sample.txt", sep=",")
-# qod = QoD (data, target="label")
+		plt.show ()
 
+	def plot_top20(self, col):
+		stats = self.df[[col, self.target]].groupby(col).agg(['count', 'mean', 'sum'])
+		stats = stats.reset_index()
+		stats.columns = [col, 'count', 'mean', 'sum']
+		stats = stats.sort_values('count', ascending=False)
+		fig, ax1 = plt.subplots(figsize=(15,4))
+		ax2 = ax1.twinx()
+		ax1.bar(stats[col].astype(str).values[0:20], stats['count'].values[0:20])
+		ax1.set_xticklabels(stats[col].astype(str).values[0:20], rotation='vertical')
+		ax2.plot(stats['mean'].values[0:20], color='red')
+		ax2.set_ylim(0,1)
+		ax2.set_ylabel('Mean Target')
+		ax1.set_ylabel('Frequency')
+		ax1.set_xlabel(col)
+		ax1.set_title('Top20 ' + col + 's based on frequency')
+
+		plt.show ()
+
+# data = pd.read_csv ("/Users/apple/AutoCTR project/dataset/Movielens/ml-100k/u.data", sep="\\s+")
+# qod = QoD (data, target="ratings")
+
+# cat = 'user'
+# te = data[[cat, 'ratings']].groupby (cat).mean ()
+# te = te.reset_index ()
+# te.columns = [cat, 'TE_' + cat]
+# data = data.merge (te, how='left', on=cat)
 
 # qod._get_correlations ()
 # # outlier_dict = qod._get_completeness ()
